27a8731eb66a7e5c254762ea8dcf938e63bf93d8
Samriddhi <samriddhi,25bai10185@vitbhopal.ac.in>
2026-06-14 15:41:19 +0530

    Add title and live digital clock

--- main.py
+++ main.py
@@ -1,8 +1,34 @@
 import tkinter as tk
+from datetime import datetime
 
 root = tk.Tk()
-
 root.title("Smart Alarm Clock")
 root.geometry("600x500")
+
+
+title = tk.Label(
+    root,
+    text="Smart Alarm Clock",
+    font=("Segoe UI", 24, "bold")
+)
+title.pack(pady=20)
+
+
+clock_label = tk.Label(
+    root,
+    text="00:00:00",
+    font=("Consolas", 32, "bold")
+)
+clock_label.pack(pady=20)
+
+
+def update_clock():
+    current_time = datetime.now().strftime("%H:%M:%S")
+    clock_label.config(text=current_time)
+
+    # Update every second
+    root.after(1000, update_clock)
+
+update_clock()
 
 root.mainloop()
